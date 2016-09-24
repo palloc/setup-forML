a851af384c22a7c4547d952d86b0602af6a76fde
palloc <palloc.mknet10@gmail.com>
2016-09-24 23:53:49 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt-get install gcc
 sudo apt-get install python-dev python-pip
-sudo pip install -r package.pip
+pip install -r package.pip
+pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0-cp27-none-linux_x86_64.whl
